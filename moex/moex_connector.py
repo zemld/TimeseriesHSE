df0c181282e6b5951143195195002c5886580ee9
Sunr1seColours <zemlyanukhind@yandex.ru>
2024-12-30 13:38:03 +0300

    Написал логику получения данных с биржи

--- moex/moex_connector.py
+++ moex/moex_connector.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
-
+import requests
+from enum import Enum
 
 class MOEXConnector:
     class MOEXRequestAttributes:
@@ -8,43 +9,71 @@ class MOEXConnector:
         _from_date: date
         _till_date: date
 
-
-        def __init__(self, ticket: str, from_date=datetime.today() - relativedelta(years=3), till_date=datetime.today()):
+        def __init__(
+            self,
+            ticket: str,
+            from_date=datetime.today() - relativedelta(years=3),
+            till_date=datetime.today(),
+        ):
             self._ticket = ticket
             self._from_date = from_date
             self._till_date = till_date
 
-        
-        def _get_start_date(self) -> date:
+        def get_start_date(self) -> date:
             return self._from_date
 
-
-        def _get_end_date(self) -> date:
+        def get_end_date(self) -> date:
             return self._till_date
-
 
         def get_ticket(self) -> str:
             return self._ticket
-        
 
-        def date_to_string(date: date) -> str:
-            return f'{date.year}-{date.month}-{date.day}'
-    
+        def date_to_string(self, date: date) -> str:
+            return f"{date.year}-{date.month}-{date.day}"
 
-    _action_request: str = 'https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/'
-    _bond_request: str = 'https://iss.moex.com/iss/history/engines/bonds/markets/shares/boards/TQBR/securities/'
+    class Format(Enum):
+        CSV = "csv"
+        JSON = "json"
 
-    
-    def _create_request(self, request_body: str, format: str, attributes: MOEXRequestAttributes):
-        from_date: date = attributes._get_start_date()
-        till_date: date = attributes._get_end_date()
-        url = request_body + attributes.get_ticket() + '.' + format + '?from=' + attributes.date_to_string(from_date) + '&till=' + attributes.date_to_string(till_date)
+    _action_request: str = (
+        "https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/"
+    )
+    _bond_request: str = (
+        "https://iss.moex.com/iss/history/engines/bonds/markets/shares/boards/TQBR/securities/"
+    )
+
+    def _create_request(
+        self, request_body: str, format: Format, attributes: MOEXRequestAttributes
+    ):
+        from_date: date = attributes.get_start_date()
+        till_date: date = attributes.get_end_date()
+        url = (
+            request_body
+            + attributes.get_ticket()
+            + '.'
+            + format
+            + '?from='
+            + attributes.date_to_string(from_date)
+            + '&till='
+            + attributes.date_to_string(till_date)
+            + '&iss.meta=off'
+        )
         return url
 
-    
-    def get_actions(self, attributes: MOEXRequestAttributes, result_format: str):
-        pass
-        
+    def get_actions(self, attributes: MOEXRequestAttributes, result_format: Format):
+        url = self._create_request(self._action_request, result_format, attributes)
+        response = requests.get(url)
+        return self.get_formatted_response(response, result_format)
 
-    def get_bonds(self, attributes: MOEXRequestAttributes, result_format: str):
-        pass
+    def get_bonds(self, attributes: MOEXRequestAttributes, result_format: Format):
+        url = self._create_request(self._bond_request, result_format, attributes)
+        response = requests.get(url)
+        return self.get_formatted_response(response, result_format)
+    
+    def get_formatted_response(self, response: requests.Response, format: Format):
+        # У Responce есть возможность вернуть данные только либо как текст, либо как json.
+        # if format == self.Format.CSV.value:
+        #     return response.csv()
+        
+        return response.text
+
