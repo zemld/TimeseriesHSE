8aba24435f426061facf51a86da1beb4420847a6
Sunr1seColours <zemlyanukhind@yandex.ru>
2024-12-30 13:46:37 +0300

    Убрал ненужную логику с форматом возвращаемого значения

--- moex/moex_connector.py
+++ moex/moex_connector.py
@@ -3,6 +3,7 @@ from dateutil.relativedelta import relativedelta
 import requests
 from enum import Enum
 
+
 class MOEXConnector:
     class MOEXRequestAttributes:
         _ticket: str
@@ -31,10 +32,6 @@ class MOEXConnector:
         def date_to_string(self, date: date) -> str:
             return f"{date.year}-{date.month}-{date.day}"
 
-    class Format(Enum):
-        CSV = "csv"
-        JSON = "json"
-
     _action_request: str = (
         "https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/TQBR/securities/"
     )
@@ -42,38 +39,27 @@ class MOEXConnector:
         "https://iss.moex.com/iss/history/engines/bonds/markets/shares/boards/TQBR/securities/"
     )
 
-    def _create_request(
-        self, request_body: str, format: Format, attributes: MOEXRequestAttributes
-    ):
+    def _create_request(self, request_body: str, attributes: MOEXRequestAttributes):
         from_date: date = attributes.get_start_date()
         till_date: date = attributes.get_end_date()
         url = (
             request_body
             + attributes.get_ticket()
-            + '.'
-            + format
-            + '?from='
+            + ".json"
+            + "?from="
             + attributes.date_to_string(from_date)
-            + '&till='
+            + "&till="
             + attributes.date_to_string(till_date)
-            + '&iss.meta=off'
+            + "&iss.meta=off"
         )
         return url
 
-    def get_actions(self, attributes: MOEXRequestAttributes, result_format: Format):
-        url = self._create_request(self._action_request, result_format, attributes)
+    def get_actions(self, attributes: MOEXRequestAttributes):
+        url = self._create_request(self._action_request, attributes)
         response = requests.get(url)
-        return self.get_formatted_response(response, result_format)
+        return response.json()
 
-    def get_bonds(self, attributes: MOEXRequestAttributes, result_format: Format):
-        url = self._create_request(self._bond_request, result_format, attributes)
+    def get_bonds(self, attributes: MOEXRequestAttributes):
+        url = self._create_request(self._bond_request, attributes)
         response = requests.get(url)
-        return self.get_formatted_response(response, result_format)
-    
-    def get_formatted_response(self, response: requests.Response, format: Format):
-        # У Responce есть возможность вернуть данные только либо как текст, либо как json.
-        # if format == self.Format.CSV.value:
-        #     return response.csv()
-        
-        return response.text
-
+        return response.json()
