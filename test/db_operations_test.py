b23aefa5668b0104c1ca35aee2362f25f5f3fc7e
Sunr1seColours <zemlyanukhind@yandex.ru>
2025-01-09 18:15:15 +0300

    Написал тесты для операций с базой данных

--- test/db_operations_test.py
+++ test/db_operations_test.py
@@ -1,12 +1,14 @@
+from datetime import date
+from db_connection import db_connector as dbc
 import pytest
 from unittest.mock import AsyncMock
-from db_connection import db_connector as dbc
 
 
 @pytest.fixture
 def connector():
     conn = dbc.DBConnector("host", 5432, "name", "user", "password")
     conn._pool = AsyncMock()
+    conn._check_and_create_connetion = AsyncMock()
     return conn
 
 
@@ -32,9 +34,91 @@ async def test_check_and_create_connetion_when_pool_exists(mocker, connector):
     assert not mock_connect.connect.called
 
 
+# TODO: исправить тесты
 @pytest.mark.asyncio
 async def test_check_and_create_connetion_when_pool_does_not_exist(mocker, connector):
-    mock_connect = mocker.patch("db_connection.db_connector.DBConnector.connect")
+    mocked_connect = AsyncMock()
+    connector.connect = mocked_connect
     connector._pool = None
     await connector._check_and_create_connetion()
-    mock_connect.assert_called_once()
+    mocked_connect.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_create_table(mocker, connector):
+    mocked_pool = AsyncMock()
+    connector._pool = mocked_pool
+
+    mock_acquire = AsyncMock()
+    mocked_pool.acquire.return_value = mock_acquire
+
+    mocked_connection = AsyncMock()
+    mock_acquire.__aenter__.return_value = mocked_connection
+    mocked_connection.execute = AsyncMock()
+
+    await connector.create_table("TEST_TABLE")
+    mocked_connection.execute.assert_called_once_with(
+        """
+        CREATE TABLE IF NOT EXISTS TEST_TABLE (
+            date DATE PRIMARY KEY,
+            price DOUBLE
+        );
+        """
+    )
+
+
+@pytest.mark.asyncio
+async def test_insert_data(mocker, connector):
+    mocked_pool = AsyncMock()
+    connector._pool = mocked_pool
+
+    mock_acquire = AsyncMock()
+    mocked_pool.acquire.return_value = mock_acquire
+
+    mocked_connection = AsyncMock()
+    mock_acquire.__aenter__.return_value = mocked_connection
+    mocked_connection.execute = AsyncMock()
+
+    await connector.insert_data("TEST_TABLE", {"2022-01-01": 1.0, "2022-01-02": 2.0})
+    mocked_connection.execute.assert_called_once_with(
+        "INSERT INTO TEST_TABLE (date, price) VALUES\n\t('2022-01-01', 1.0),\n\t('2022-01-02', 2.0);"
+    )
+
+
+@pytest.mark.asyncio
+async def test_select_data(mocker, connector):
+    mocked_pool = AsyncMock()
+    connector._pool = mocked_pool
+
+    mock_acquire = AsyncMock()
+    mocked_pool.acquire.return_value = mock_acquire
+
+    mocked_connection = AsyncMock()
+    mock_acquire.__aenter__.return_value = mocked_connection
+    mocked_connection.fetch.return_value = {"2022-01-01": 1.0, "2022-01-02": 2.0}
+
+    result = await connector.select_data(
+        "TEST_TABLE", date(2022, 1, 1), date(2022, 1, 2)
+    )
+    assert result == {"2022-01-01": 1.0, "2022-01-02": 2.0}
+    mocked_connection.fetch.assert_called_once_with(
+        "SELECT * FROM TEST_TABLE WHERE date BETWEEN '2022-01-01' AND '2022-01-02';"
+    )
+
+
+@pytest.mark.asyncio
+async def test_delete_data(mocker, connector):
+    mocked_pool = AsyncMock()
+    connector._pool = mocked_pool
+
+    mock_acquire = AsyncMock()
+    mocked_pool.acquire.return_value = mock_acquire
+
+    mocked_connection = AsyncMock()
+    mock_acquire.__aenter__.return_value = mocked_connection
+    mocked_connection.execute = AsyncMock()
+
+    await connector.delete_data("TEST_TABLE", date(2022, 1, 2))
+    mocked_connection.execute.assert_called_once_with(
+        "DELETE FROM TEST_TABLE WHERE date <= '2022-01-02';"
+    )
