90c0fe4a5627cf8db327a8f688c7bb5f4cb17ff0
Sunr1seColours <zemlyanukhind@yandex.ru>
2025-01-28 15:00:53 +0300

    Создан логгер

---
+++ logger.py
@@ -0,0 +1,27 @@
+import logging
+
+
+class Logger:
+    logger: logging.Logger
+
+    def __init__(self, name: str):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+
+        handler = logging.FileHandler(f"{name}.log", mode="w")
+
+        formatter = logging.Formatter(
+            "%(asctime)s || %(name)s || %(levelname)s || %(message)s"
+        )
+        handler.setFormatter(formatter)
+
+        self.logger.addHandler(handler)
+
+    def debug(self, message: str):
+        self.logger.debug(message)
+
+    def info(self, message: str):
+        self.logger.info(message)
+
+    def error(self, message: str):
+        self.logger.error(message)
