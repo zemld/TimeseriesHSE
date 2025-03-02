af4280f420260cc4138d5e94091404ac533b992e
Sunr1seColours <zemlyanukhind@yandex.ru>
2025-03-02 11:38:13 +0300

    Изменен параметр запроса

--- services/db/main.py
+++ services/db/main.py
@@ -61,11 +61,11 @@ async def delete_data(request: DeleteDataRequest):
 
 
 @db_manager.get("/select_data")
-async def select_data(table_name: str, from_date: str, till_date: str):
-    logger.info(f"Recieved request to select data from table {table_name}")
+async def select_data(request: SelectDataRequest):
+    logger.info(f"Recieved request to select data from table {request.table_name}")
     try:
-        data = await db.select_data(table_name, from_date, till_date)
+        data = await db.select_data(request.table_name, request.from_date, request.till_date)
         return {"data": data}
     except Exception as e:
-        logger.error(f"Failed to select data from table {table_name}: {str(e)}")
+        logger.error(f"Failed to select data from table {request.table_name}: {str(e)}")
         raise HTTPException(status_code=500, detail=str(e))
