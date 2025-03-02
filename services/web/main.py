cda78c5de205bf9bf310242db2ddff4dfb90816c
Sunr1seColours <zemlyanukhind@yandex.ru>
2025-03-02 11:40:56 +0300

    Изменена логика обработки запроса

--- services/web/main.py
+++ services/web/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 import httpx
 from logger import Logger
@@ -8,40 +9,53 @@ templates = Jinja2Templates(directory="templates")
 logger = Logger("webapp")
 
 
+async def fetch_parameters(request: Request):
+    parameters = await request.json()
+    value = parameters.get("value")
+    start_date = parameters.get("start_date")
+    end_date = parameters.get("end_date")
+    return (value, start_date, end_date)
+
+
 @webapp.get("/")
 async def start(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@webapp.post("/chart-parameters")
-async def handle_parameters(request: Request):
-    parameters = await request.json()
-    value = parameters.get("value")
-    start_date = parameters.get("start_date")
-    end_date = parameters.get("end_date")
+@webapp.get("/make-analysis")
+async def run_flow(request: Request):
+    select_data_parameters = await fetch_parameters(request)
+    logger.debug(f"Sending request with params: {select_data_parameters}")
 
-    logger.info(f"Got parameters: {value}, {start_date}, {end_date}")
+    try:
+        async with httpx.AsyncClient() as client:
+            db_response = await client.get(
+                "http://db_service:8002/select_data",
+                params=dict(
+                    zip(
+                        ["table_name", "from_date", "till_date"], select_data_parameters
+                    )
+                ),
+            )
+            logger.info(f"Got info: {db_response.text}")
 
-    redirect_url = (
-        str(request.url_for("show_chart"))
-        + f"?value={value}&start_date={start_date}&end_date={end_date}"
-    )
+            data = db_response.get("data")
+            if not data:
+                raise Exception("No data found")
 
-    logger.info(f"Make redirect for {redirect_url}")
+            logger.debug(f"Sending data to analyzer: {data}")
+            analize_response = await client.get(
+                "http://analyzer:8003/analize", params={"data": data}
+            )
+            predict_response = await client.get(
+                "http://analyzer:8003/predict", params={"data": data}
+            )
+    except Exception as e:
+        logger.error(e)
+        return RedirectResponse(url=f"/error?error={e}")
 
-    return {"redirect": redirect_url}
 
-
-@webapp.get("/chart")
-async def show_chart(request: Request):
-    parameters = request.query_params
-    value = parameters.get("value")
-    from_date = parameters.get("start_date")
-    till_date = parameters.get("end_date")
-    params = {"table_name": value, "from_date": from_date, "till_date": till_date}
-
-    logger.debug(f"Sending request with params: {params}")
-    r = httpx.get("http://db_service:8002/select_data", params=params)
-
-    logger.info(f"Got info: {r.text}")
-    return f"{r.text}"
+@webapp.get("/error")
+async def handle_error(request: Request):
+    error = request.get("error")
+    return f"Error: {error}"
