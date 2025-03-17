bede47b1dac24c0f4e620636607ed71e4f741efd
Sunr1seColours <zemlyanukhind@yandex.ru>
2025-03-17 09:59:27 +0300

    Добавлена логика выбора данных

--- services/electricity/main.py
+++ services/electricity/main.py
@@ -11,9 +11,17 @@ async def fetch(year: str, month: str, day: str):
     try:
         response = await httpx.get(url)
         response.raise_for_status()
-        data = response.json()
+        data = choose_data(response.json())
         logger.info(f"Collected data: {data}")
         return data
     except httpx.HTTPError as e:
         logger.error(f"Failed to fetch data: {e}")
         return {"error": str(e)}
+    
+def choose_data(data: dict):
+    chosen_data = {}
+    for record in data:
+        timestamp = record["time_start"]
+        price = record["EUR_per_kWh"]
+        chosen_data[timestamp] = price
+    return chosen_data
