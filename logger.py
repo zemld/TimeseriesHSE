3542f0e42cd5b7736de9d8fd835c523d3ba9b1c2
Sunr1seColours <zemlyanukhind@yandex.ru>
2025-03-31 12:15:08 +0300

    Добавлена возможность записать warning

--- logger.py
+++ logger.py
@@ -23,5 +23,8 @@ class Logger:
     def info(self, message: str):
         self.logger.info(message)
 
+    def warning(self, message: str):
+        self.logger.warning(message)
+
     def error(self, message: str):
         self.logger.error(message)
